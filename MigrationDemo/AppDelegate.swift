c550507d0d9521481d0333e178aec3295000ed2d
Asmaa <asmaatarek93@gmail.com>
2023-06-15 02:50:01 +0300

    Remove setting rootVC from AppDelegate

--- MigrationDemo/AppDelegate.swift
+++ MigrationDemo/AppDelegate.swift
@@ -11,11 +11,6 @@ import UIKit
 class AppDelegate: UIResponder, UIApplicationDelegate {
     var window: UIWindow?
     func application(_ application: UIApplication, didFinishLaunchingWithOptions launchOptions: [UIApplication.LaunchOptionsKey: Any]?) -> Bool {
-        let storyboard = UIStoryboard.init(name: "Main", bundle: .main)
-        window = UIWindow(frame: UIScreen.main.bounds)
-        // or initialize from nib file
-        window?.rootViewController = storyboard.instantiateInitialViewController()
-        window?.makeKeyAndVisible()
         return true
     }
 }
