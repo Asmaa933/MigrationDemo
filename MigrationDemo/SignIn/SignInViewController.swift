5f39078b562189d4530d16a1037abdfa409084ca
Asmaa <asmaatarek93@gmail.com>
2023-06-15 03:00:45 +0300

    Add SignInViewController using xib and make it rootVC from SceneDelegate

---
+++ MigrationDemo/SignIn/SignInViewController.swift
@@ -0,0 +1,19 @@
+//
+//  SignInViewController.swift
+//  MigrationDemo
+//
+//  Created by Asmaa Tarek on 15/06/2023.
+//
+
+import UIKit
+
+class SignInViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+    }
+
+    @IBAction private func loginAction(_ sender: UIButton) {
+    }
+}
